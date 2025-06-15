5d13f211d02788ff5340b67c9658d6878ca568bb
syakil <syakilahmadginanjar@gmail.com>
2025-06-15 13:36:18 +0700

    first commit

---
+++ hris/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace hris.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
